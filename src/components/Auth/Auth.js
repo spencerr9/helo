7e991f42a9efd910d127f4db056aae884267f39e
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2018-07-27 16:34:33 -0600

    final commit for the day

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,22 +1,43 @@
 import React, {Component} from 'react';
-import {updateUsername} from '../../ducks/reducer';
-import {updatePassword} from '../../ducks/reducer';
+import { connect } from 'react-redux';
+import {updateUsername, updatePassword} from '../../ducks/reducer';
+import axios from 'axios';
 
-export default class Auth extends Component {
+class Auth extends Component {
+
+    register(){
+        // const {username, password} = req.body
+        axios.post('/api/register').then(res => {
+            console.log(true)
+        })
+    }
 
     // axios.post('/api/register').then((res) => {
     //     this.setState({})
     // })
 
     render(){
+        const {updateUsername, updatePassword} = this.props
+
         return(
             <div>
                 <h1>Auth</h1>
                 <input placeholder="username" type="text" onChange={(e) => updateUsername(e.target.value)} />
                 <input placeholder="password" type="text" onChange={(e) => updatePassword(e.target.value)} />
                 <button>Login</button>
-                <button>Register</button>
+                <button onClick={() => this.register()}>Register</button>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    const {username, password} = state;
+
+    return {
+        username,
+        password
+    };
+}
+
+export default connect(mapStateToProps, {updateUsername, updatePassword})(Auth);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './ducks/store'
 
 ReactDOM.render(
-    <HashRouter store={store}>
-        <App />
-    </HashRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>
 , document.getElementById('root'));
 registerServiceWorker();
